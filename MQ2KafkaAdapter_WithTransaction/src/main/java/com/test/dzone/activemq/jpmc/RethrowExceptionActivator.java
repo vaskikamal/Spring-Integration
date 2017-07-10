e9063debf8c3c828a410c01e302ad8a8800c80ab
vaskikamal@gmail.com <vaskikamal@gmail.com>
2017-07-10 23:41:47 +0530

    Working commit

---
+++ MQ2KafkaAdapter_WithTransaction/src/main/java/com/test/dzone/activemq/jpmc/RethrowExceptionActivator.java
@@ -0,0 +1,14 @@
+package com.test.dzone.activemq.jpmc;
+
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+@MessageEndpoint
+public class RethrowExceptionActivator {
+	@ServiceActivator(inputChannel = "rethrowChannel")
+	public void rethrow(Message<Exception> msg) throws Exception {
+		throw msg.getPayload();
+	}
+
+}
---
+++ MQ2KafkaAdapter_WithTransaction/src/main/java/com/test/dzone/activemq/jpmc/SayHelloService.java
@@ -0,0 +1,30 @@
+package com.test.dzone.activemq.jpmc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+@MessageEndpoint
+public class SayHelloService {
+
+	private final Logger logger = LoggerFactory.getLogger(SayHelloService.class);
+
+	@ServiceActivator(inputChannel = "helloChannel", outputChannel="inputToKafka")
+	public Message<?> sayHello(Message<?> name) {
+		System.out.println("################################");
+		System.out.println("################################");
+		System.out.println("################################");
+		System.out.println("##  " +  name.getPayload() + "!!!");
+		System.out.println("##  " +  name.getHeaders() + "!!!");
+
+		System.out.println("################################");
+		
+		System.out.println("################################");
+		System.out.println("################################");
+		
+		return name;
+		
+	}
+}
